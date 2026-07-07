cf0a6fc438bc19334eb1ab22fa239f9fb88c3338
some1likeKang <anzhikang6@163.com>
2026-07-07 08:54:16 +0800

    add lm_dataset: PretrainDataset

---
+++ dataset/lm_dataset.py
@@ -0,0 +1,57 @@
+from torch.utils.data import Dataset
+import torch
+import json
+import os
+import random
+from datasets import load_dataset, Features, Sequence, Value
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+def pre_processing_chat(conversations, add_system_ratio=0.2):
+    # tool use 数据完整保留不做处理
+    if any(conv.get('tools') for conv in conversations): return conversations
+
+    SYSTEM_PROMPTS = [
+        "你是一个知识丰富的AI，尽力为用户提供准确的信息。",
+        "你是minimind，一个小巧但有用的语言模型。",
+        "你是一个专业的AI助手，请提供有价值的回答。",
+        "你是minimind，请尽力帮助用户解决问题。",
+        "你是一个可靠的AI，请给出准确的回答。",
+        "You are a helpful AI assistant.",
+        "You are minimind, a lightweight intelligent assistant.",
+        "You are a friendly chatbot. Please answer the user's questions carefully.",
+        "You are a knowledgeable AI. Try your best to provide accurate information.",
+        "You are minimind, a small but useful language model."
+    ]
+    # 概率性添加system
+    if conversations[0].get('role') != 'system':
+        if random.random() < add_system_ratio:
+            return [{'role': 'system', 'content': random.choice(SYSTEM_PROMPTS)}] + conversations
+    return conversations
+
+def post_processing_chat(prompt_content, empty_think_ratio=0.2):
+    # 以80%概率移除空思考标签
+    if '<think>\n\n</think>\n\n' in prompt_content and random.random() > empty_think_ratio:
+        prompt_content = prompt_content.replace('<think>\n\n</think>\n\n', '')
+    return prompt_content
+
+
+class PretrainDataset(Dataset):
+    def __init__(self,data_path,tokenizer,max_length = 512):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.samples = load_dataset('json',data_files = data_path,split = 'train')
+
+    def __len__(self):
+        return len(self.samples)
+    
+    def __getitem__(self,index):
+        sample = self.samples[index]
+        tokens = self.tokenizer(str(sample['text']),add_special_tokens = False,max_length = self.max_length - 2,truncation = True).input_ids
+        tokens = [self.tokenizer.bos_token_id]+tokens+[self.tokenizer.eos_token_id]
+        input_ids = tokens + [self.tokenizer.pad_token_id]*(self.max_length - len(tokens))
+        input_ids = torch.tensor(input_ids,dtype=torch.long)
+        labels = input_ids.clone()
+        labels[input_ids == self.tokenizer.pad_token_id] = -100
+        return input_ids,labels
+
