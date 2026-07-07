cc38aabea67304c3830eaa2870f6f7aeafa8fa5c
some1likeKang <anzhikang6@163.com>
2026-07-07 13:43:32 +0800

    add pretrain part and some trainer utils

--- trainer/trainer_utils.py
+++ trainer/trainer_utils.py
@@ -1,1 +1,21 @@
 #主要是放置一些通用的工具类函数
+"""
+训练工具函数集合
+"""
+import os
+import sys
+__package__ = "trainer"
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import random
+import math
+import numpy as np
+import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data import Sampler
+from transformers import AutoTokenizer, AutoModel, AutoModelForSequenceClassification
+from model.model_minimind import MiniMindForCausalLM
+
+
+def get_lr(current_step, total_steps, lr):
+    return lr*(0.1 + 0.45*(1 + math.cos(math.pi * current_step / total_steps)))
